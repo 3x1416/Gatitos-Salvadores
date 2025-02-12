4b2fd2947f2da6d9742121c5aee5214a7ec3b17b
3x1416 <elieceralmora2020@gmail.com>
2025-02-11 19:16:11 -0600

    Create bot_logic.py

---
+++ bot_logic.py
@@ -0,0 +1,43 @@
+import random
+import requests
+
+def gen_pass(length):
+    #lista de caracteres
+    symbols =  "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    #variable de la contraseña
+    password = ''
+
+    for i in range(length): 
+        password += random.choice(symbols)
+
+    return password
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "HEADS"
+    else:
+        return "TAILS"
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
